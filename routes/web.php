ecef94cd038518ab18df9d40763bb9963bc15c10
ArtemD <>
2025-05-08 04:30:27 +0300

    auth settings update

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,10 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
